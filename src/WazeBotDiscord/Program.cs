9045932f5e91b06f579ea7e5b600342bd158147c
Steven Hall <vaindil@gmail.com>
2017-04-06 18:20:57 -0400

    initial commit

---
+++ src/WazeBotDiscord/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WazeBotDiscord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
