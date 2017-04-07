724447d57a36681a221b8699874fc2430937217e
Steven Hall <vaindil@gmail.com>
2017-04-07 12:22:42 -0400

    setup done, initial test command added

--- src/WazeBotDiscord/Program.cs
+++ src/WazeBotDiscord/Program.cs
@@ -1,12 +1,70 @@
-﻿using System;
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
 
 namespace WazeBotDiscord
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        DiscordSocketClient client;
+        CommandService commands;
+        DependencyMap map;
+
+        public static void Main(string[] args)
+            => new Program().RunAsync().GetAwaiter().GetResult();
+
+        public async Task RunAsync()
         {
-            Console.WriteLine("Hello World!");
+            var token = Environment.GetEnvironmentVariable("DISCORD_API_TOKEN");
+            if (token == null)
+                throw new ArgumentNullException(nameof(token), "No Discord API token env var found");
+
+            client = new DiscordSocketClient();
+
+            var commandsConfig = new CommandServiceConfig
+            {
+                CaseSensitiveCommands = false,
+            };
+
+            commands = new CommandService(commandsConfig);
+            map = new DependencyMap();
+            await InstallCommands();
+
+            client.Log += Log;
+
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        public async Task InstallCommands()
+        {
+            client.MessageReceived += HandleCommand;
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        public async Task HandleCommand(SocketMessage messageParam)
+        {
+            var message = messageParam as SocketUserMessage;
+            if (message == null)
+                return;
+
+            int argPos = 0;
+            if (!(message.HasCharPrefix('!', ref argPos) || message.HasMentionPrefix(client.CurrentUser, ref argPos)))
+                return;
+
+            var context = new CommandContext(client, message);
+            var result = await commands.ExecuteAsync(context, argPos, map);
+        }
+
+        Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
