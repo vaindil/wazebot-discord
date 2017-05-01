ed417dcb5eda3010f50f5e61e2b6e1d04f129d22
Steven Hall <vaindil@gmail.com>
2017-05-01 08:36:29 -0400

    [untested] add role ID module

---
+++ src/WazeBotDiscord/Modules/RoleIdModule.cs
@@ -0,0 +1,37 @@
+﻿using Discord;
+using Discord.Commands;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WazeBotDiscord.Utilities;
+
+namespace WazeBotDiscord.Modules
+{
+    [Group("roleid")]
+    [RequireLcOrAbove]
+    public class RoleIdModule : ModuleBase
+    {
+        [Command]
+        public async Task GetSpecific(IRole role) => await ReplyAsync($"Role {role.Name}: {role.Id}");
+
+        [Command("wazeall")]
+        public async Task GetAll()
+        {
+            var reply = new StringBuilder("__Waze roles on this server__");
+
+            foreach (var name in WazeRoleNames.RoleNames)
+            {
+                var idString = "";
+                var role = Context.Guild.Roles.FirstOrDefault(r => r.Name == name);
+                if (role == null)
+                    idString = "(role not present)";
+                else
+                    idString = role.Id.ToString();
+
+                reply.Append($"\n{name}: {idString}");
+            }
+
+            await ReplyAsync(reply.ToString());
+        }
+    }
+}
---
+++ src/WazeBotDiscord/Utilities/WazeRoleNames.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace WazeBotDiscord.Utilities
+{
+    public static class WazeRoleNames
+    {
+        public static readonly IReadOnlyCollection<string> RoleNames = new ReadOnlyCollection<string>(new List<string>
+        {
+            "Admin",
+            "Admin Keyholder",
+            "Regional Coordinator",
+            "Assistant Regional Coordinator",
+            "Global Champ",
+            "Local Champ",
+            "Bot",
+            "Country Manager",
+            "State Manager",
+            "Large Area Manager",
+            "Area Manager",
+            "Mentor",
+            "Level 6",
+            "Level 5",
+            "Level 4",
+            "Level 3",
+            "Level 2",
+            "Level 1"
+        });
+    }
+}
