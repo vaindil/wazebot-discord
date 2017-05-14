f82caa7b85d8cb10b627fa5fdf21d6d6759a1163
Steven Hall <vaindil@gmail.com>
2017-05-14 17:18:55 -0400

    add MAR IDs

--- src/WazeBotDiscord/Regions/MidAtlantic.cs
+++ src/WazeBotDiscord/Regions/MidAtlantic.cs
@@ -2,32 +2,30 @@
 {
     public static class MidAtlanticChannels
     {
-        public const ulong StateManagers = 0;
-        public const ulong LargeAreaManagers = 0;
-        public const ulong AreaManagers = 0;
-        public const ulong DistrictOfColumbia = 0;
-        public const ulong Maryland = 0;
-        public const ulong Virginia = 0;
-        public const ulong WestVirginia = 0;
+        public const ulong StateManagers = 299679934216011777;
+        public const ulong AreaManagers = 299679772936765441;
+        public const ulong DistrictOfColumbia = 299679270307889172;
+        public const ulong Maryland = 299679215295660032;
+        public const ulong Virginia = 299679182802255873;
+        public const ulong WestVirginia = 299679140808753152;
 }
 
     public static class MidAtlanticRoles
     {
-        public const ulong MAR = 0;
-        public const ulong CountryManager = 0;
-        public const ulong StateManager = 0;
-        public const ulong LargeAreaManager = 0;
-        public const ulong AreaManager = 0;
-        public const ulong Mentor = 0;
-        public const ulong Level6 = 0;
-        public const ulong Level5 = 0;
-        public const ulong Level4 = 0;
-        public const ulong Level3 = 0;
-        public const ulong Level2 = 0;
-        public const ulong Level1 = 0;
-        public const ulong DistrictOfColumbia = 0;
-        public const ulong Maryland = 0;
-        public const ulong Virginia = 0;
-        public const ulong WestVirginia = 0;
+        public const ulong MAR = 313424101291065356;
+        public const ulong CountryManager = 299677234787123200;
+        public const ulong StateManager = 299677955540647937;
+        public const ulong AreaManager = 299678085706547210;
+        public const ulong Mentor = 313424374524936202;
+        public const ulong Level6 = 299681069546995712;
+        public const ulong Level5 = 299681057886699523;
+        public const ulong Level4 = 299681046746628096;
+        public const ulong Level3 = 299681033647816704;
+        public const ulong Level2 = 299681014530310147;
+        public const ulong Level1 = 299680910142603274;
+        public const ulong DistrictOfColumbia = 299681180012511232;
+        public const ulong Maryland = 299681164262637568;
+        public const ulong Virginia = 299681141948940289;
+        public const ulong WestVirginia = 299681085560979457;
 }
 }
