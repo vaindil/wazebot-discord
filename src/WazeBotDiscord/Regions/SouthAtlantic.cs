8832a07e2ecb2d39172047f08a5866d5b422971a
Steven Hall <vaindil@gmail.com>
2017-05-14 17:10:00 -0400

    SAR channels

--- src/WazeBotDiscord/Regions/SouthAtlantic.cs
+++ src/WazeBotDiscord/Regions/SouthAtlantic.cs
@@ -2,32 +2,30 @@
 {
     public static class SouthAtlanticChannels
     {
-        public const ulong StateManagers = 0;
-        public const ulong LargeAreaManagers = 0;
-        public const ulong AreaManagers = 0;
-        public const ulong Kentucky = 0;
-        public const ulong NorthCarolina = 0;
-        public const ulong SouthCarolina = 0;
-        public const ulong Tennessee = 0;
+        public const ulong StateManagers = 300760513808171019;
+        public const ulong AreaManagers = 300760910631534592;
+        public const ulong Kentucky = 300761676817629184;
+        public const ulong NorthCarolina = 300761841502781440;
+        public const ulong SouthCarolina = 300761935555854336;
+        public const ulong Tennessee = 300761715396837377;
     }
 
     public static class SouthAtlanticRoles
     {
-        public const ulong SAR = 0;
-        public const ulong CountryManager = 0;
-        public const ulong StateManager = 0;
-        public const ulong LargeAreaManager = 0;
-        public const ulong AreaManager = 0;
-        public const ulong Mentor = 0;
-        public const ulong Level6 = 0;
-        public const ulong Level5 = 0;
-        public const ulong Level4 = 0;
-        public const ulong Level3 = 0;
-        public const ulong Level2 = 0;
-        public const ulong Level1 = 0;
-        public const ulong Kentucky = 0;
-        public const ulong NorthCarolina = 0;
-        public const ulong SouthCarolina = 0;
-        public const ulong Tennessee = 0;
+        public const ulong SAR = 313421038539243533;
+        public const ulong CountryManager = 300756688171040779;
+        public const ulong StateManager = 300755483726446592;
+        public const ulong AreaManager = 300757047534813194;
+        public const ulong Mentor = 300763117019725825;
+        public const ulong Level6 = 313420729007996928;
+        public const ulong Level5 = 300770016750469132;
+        public const ulong Level4 = 300769886735564801;
+        public const ulong Level3 = 300769754543816715;
+        public const ulong Level2 = 300769620749582336;
+        public const ulong Level1 = 300769455560982539;
+        public const ulong Kentucky = 300757720171282433;
+        public const ulong NorthCarolina = 300758074891960320;
+        public const ulong SouthCarolina = 300758019719823370;
+        public const ulong Tennessee = 300757934412136449;
     }
 }
