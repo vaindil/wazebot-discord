5902e9a738ce91b53e17a6041d6d428a3b8fa8ee
Steven Hall <vaindil@gmail.com>
2017-05-14 17:52:08 -0400

    add SCR IDs

--- src/WazeBotDiscord/Regions/Regions.cs
+++ src/WazeBotDiscord/Regions/Regions.cs
@@ -6,7 +6,7 @@
         public const ulong Northwest = 0;
         public const ulong Southwest = 301113669696356352;
         public const ulong Plains = 0;
-        public const ulong SouthCentral = 0;
+        public const ulong SouthCentral = 313431377724964876;
         public const ulong GreatLakes = 299563059695976451;
         public const ulong SouthAtlantic = 300737538384199681;
         public const ulong Southeast = 313428729739083776;
--- src/WazeBotDiscord/Regions/SouthCentral.cs
+++ src/WazeBotDiscord/Regions/SouthCentral.cs
@@ -2,34 +2,34 @@
 {
     public static class SouthCentralChannels
     {
-        public const ulong StateManagers = 0;
-        public const ulong LargeAreaManagers = 0;
-        public const ulong AreaManagers = 0;
-        public const ulong Arkansas = 0;
-        public const ulong Louisiana = 0;
-        public const ulong Mississippi = 0;
-        public const ulong Oklahoma = 0;
-        public const ulong Texas = 0;
+        public const ulong StateManagers = 313432338883411969;
+        public const ulong LargeAreaManagers = 313432374253846528;
+        public const ulong AreaManagers = 313432426011688960;
+        public const ulong Arkansas = 313432452444192788;
+        public const ulong Louisiana = 313432475449688075;
+        public const ulong Mississippi = 313432501467217922;
+        public const ulong Oklahoma = 313432519229964289;
+        public const ulong Texas = 313432534723592193;
     }
 
     public static class SouthCentralRoles
     {
-        public const ulong SCR = 0;
-        public const ulong CountryManager = 0;
-        public const ulong StateManager = 0;
-        public const ulong LargeAreaManager = 0;
-        public const ulong AreaManager = 0;
-        public const ulong Mentor = 0;
-        public const ulong Level6 = 0;
-        public const ulong Level5 = 0;
-        public const ulong Level4 = 0;
-        public const ulong Level3 = 0;
-        public const ulong Level2 = 0;
-        public const ulong Level1 = 0;
-        public const ulong Arkansas = 0;
-        public const ulong Louisiana = 0;
-        public const ulong Mississippi = 0;
-        public const ulong Oklahoma = 0;
-        public const ulong Texas = 0;
+        public const ulong SCR = 313431885181091860;
+        public const ulong CountryManager = 313431784572452865;
+        public const ulong StateManager = 313431806168793099;
+        public const ulong LargeAreaManager = 313431828478558208;
+        public const ulong AreaManager = 313431851194908683;
+        public const ulong Mentor = 313431865979830274;
+        public const ulong Level6 = 313431922325848065;
+        public const ulong Level5 = 313431942278283274;
+        public const ulong Level4 = 313431952126377994;
+        public const ulong Level3 = 313431962524188672;
+        public const ulong Level2 = 313431976524906497;
+        public const ulong Level1 = 313431989531181061;
+        public const ulong Arkansas = 313432109547257856;
+        public const ulong Louisiana = 313432134616350732;
+        public const ulong Mississippi = 313432157639147531;
+        public const ulong Oklahoma = 313432181324251138;
+        public const ulong Texas = 313432199925858304;
     }
 }
