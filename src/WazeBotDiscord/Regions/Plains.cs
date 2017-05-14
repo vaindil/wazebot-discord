020609b0a530c08d605d999262dcce75a57fc53f
Steven Hall <vaindil@gmail.com>
2017-05-14 18:01:46 -0400

    add plains IDs

--- src/WazeBotDiscord/Regions/Plains.cs
+++ src/WazeBotDiscord/Regions/Plains.cs
@@ -2,38 +2,38 @@
 {
     public static class PlainsChannels
     {
-        public const ulong StateManagers = 0;
-        public const ulong LargeAreaManagers = 0;
-        public const ulong AreaManagers = 0;
-        public const ulong Iowa = 0;
-        public const ulong Kansas = 0;
-        public const ulong Minnesota = 0;
-        public const ulong Missouri = 0;
-        public const ulong Nebraska = 0;
-        public const ulong NorthDakota = 0;
-        public const ulong SouthDakota = 0;
+        public const ulong StateManagers = 313434433753710593;
+        public const ulong LargeAreaManagers = 313434463461834752;
+        public const ulong AreaManagers = 313434510521794561;
+        public const ulong Iowa = 313434528469352448;
+        public const ulong Kansas = 313434544583868426;
+        public const ulong Minnesota = 313434565287084032;
+        public const ulong Missouri = 313434589232365568;
+        public const ulong Nebraska = 313434607695560704;
+        public const ulong NorthDakota = 313434644651573248;
+        public const ulong SouthDakota = 313434665744728064;
     }
 
     public static class PlainsRoles
     {
-        public const ulong Plains = 0;
-        public const ulong CountryManager = 0;
-        public const ulong StateManager = 0;
-        public const ulong LargeAreaManager = 0;
-        public const ulong AreaManager = 0;
-        public const ulong Mentor = 0;
-        public const ulong Level6 = 0;
-        public const ulong Level5 = 0;
-        public const ulong Level4 = 0;
-        public const ulong Level3 = 0;
-        public const ulong Level2 = 0;
-        public const ulong Level1 = 0;
-        public const ulong Iowa = 0;
-        public const ulong Kansas = 0;
-        public const ulong Minnesota = 0;
-        public const ulong Missouri = 0;
-        public const ulong Nebraska = 0;
-        public const ulong NorthDakota = 0;
-        public const ulong SouthDakota = 0;
+        public const ulong Plains = 313433884534636545;
+        public const ulong CountryManager = 313433692934635523;
+        public const ulong StateManager = 313433730385444864;
+        public const ulong LargeAreaManager = 313433712148873216;
+        public const ulong AreaManager = 313433787583430656;
+        public const ulong Mentor = 313433864766750720;
+        public const ulong Level6 = 313433908400226305;
+        public const ulong Level5 = 313433927727710210;
+        public const ulong Level4 = 313433938590957568;
+        public const ulong Level3 = 313433947910438913;
+        public const ulong Level2 = 313433958404718592;
+        public const ulong Level1 = 313433968370253824;
+        public const ulong Iowa = 313434012414771201;
+        public const ulong Kansas = 313434041942802435;
+        public const ulong Minnesota = 313434057427910656;
+        public const ulong Missouri = 313434073253281792;
+        public const ulong Nebraska = 313434089879240707;
+        public const ulong NorthDakota = 313434108212674578;
+        public const ulong SouthDakota = 313434137711214592;
     }
 }
--- src/WazeBotDiscord/Regions/Regions.cs
+++ src/WazeBotDiscord/Regions/Regions.cs
@@ -5,7 +5,7 @@
         public const ulong National = 300471946494214146;
         public const ulong Northwest = 0;
         public const ulong Southwest = 301113669696356352;
-        public const ulong Plains = 0;
+        public const ulong Plains = 313433524130545664;
         public const ulong SouthCentral = 313431377724964876;
         public const ulong GreatLakes = 299563059695976451;
         public const ulong SouthAtlantic = 300737538384199681;
