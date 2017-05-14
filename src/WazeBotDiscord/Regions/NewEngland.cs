06d5c4d7a7950ec85616338fd87679c6ebb0d7d6
Steven Hall <vaindil@gmail.com>
2017-05-14 17:30:07 -0400

    add new england IDs

--- src/WazeBotDiscord/Regions/NewEngland.cs
+++ src/WazeBotDiscord/Regions/NewEngland.cs
@@ -2,36 +2,36 @@
 {
     public static class NewEnglandChannels
     {
-        public const ulong StateManagers = 0;
-        public const ulong LargeAreaManagers = 0;
-        public const ulong AreaManagers = 0;
-        public const ulong Connecticut = 0;
-        public const ulong Maine = 0;
-        public const ulong Massachusetts = 0;
-        public const ulong NewHampshire = 0;
-        public const ulong RhodeIsland = 0;
-        public const ulong Vermont = 0;
-}
+        public const ulong StateManagers = 313426322577686529;
+        public const ulong LargeAreaManagers = 313426401736785931;
+        public const ulong AreaManagers = 313426596881104916;
+        public const ulong Connecticut = 313426679525801985;
+        public const ulong Maine = 313426698009837568;
+        public const ulong Massachusetts = 313426733883719690;
+        public const ulong NewHampshire = 313426756344479744;
+        public const ulong RhodeIsland = 313426779006173194;
+        public const ulong Vermont = 313426796848742401;
+    }
 
     public static class NewEnglandRoles
     {
-        public const ulong NER = 0;
-        public const ulong CountryManager = 0;
-        public const ulong StateManager = 0;
-        public const ulong LargeAreaManager = 0;
-        public const ulong AreaManager = 0;
-        public const ulong Mentor = 0;
-        public const ulong Level6 = 0;
-        public const ulong Level5 = 0;
-        public const ulong Level4 = 0;
-        public const ulong Level3 = 0;
-        public const ulong Level2 = 0;
-        public const ulong Level1 = 0;
-        public const ulong Connecticut = 0;
-        public const ulong Maine = 0;
-        public const ulong Massachusetts = 0;
-        public const ulong NewHampshire = 0;
-        public const ulong RhodeIsland = 0;
-        public const ulong Vermont = 0;
+        public const ulong NER = 313425686192717824;
+        public const ulong CountryManager = 313425535524929537;
+        public const ulong StateManager = 313425574204932116;
+        public const ulong LargeAreaManager = 313425600046039051;
+        public const ulong AreaManager = 313425629833854989;
+        public const ulong Mentor = 313425649362665472;
+        public const ulong Level6 = 313425685517697025;
+        public const ulong Level5 = 313425795311730689;
+        public const ulong Level4 = 313425826014298112;
+        public const ulong Level3 = 313425852010463242;
+        public const ulong Level2 = 313425867768332289;
+        public const ulong Level1 = 313425883895693313;
+        public const ulong Connecticut = 313425941894397963;
+        public const ulong Maine = 313425963809767424;
+        public const ulong Massachusetts = 313425986718793730;
+        public const ulong NewHampshire = 313426023444381696;
+        public const ulong RhodeIsland = 313426057673965570;
+        public const ulong Vermont = 313426082579742721;
     }
 }
