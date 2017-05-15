a91792520253df45512df88002901c7ded6bdda0
Steven Hall <vaindil@gmail.com>
2017-05-15 08:28:51 -0400

    add SWR IDs

--- src/WazeBotDiscord/Regions/Southwest.cs
+++ src/WazeBotDiscord/Regions/Southwest.cs
@@ -2,38 +2,36 @@
 {
     public static class SouthwestChannels
     {
-        public const ulong StateManagers = 0;
-        public const ulong LargeAreaManagers = 0;
-        public const ulong AreaManagers = 0;
-        public const ulong Arizona = 0;
-        public const ulong California = 0;
-        public const ulong Colorado = 0;
-        public const ulong Hawaii = 0;
-        public const ulong Nevada = 0;
-        public const ulong NewMexico = 0;
-        public const ulong Utah = 0;
+        public const ulong StateManagers = 302241207596351490;
+        public const ulong AreaManagers = 313652776095711232;
+        public const ulong Arizona = 302240438834954241;
+        public const ulong California = 302239929835192321;
+        public const ulong Colorado = 302240645148573697;
+        public const ulong Hawaii = 302240726291447808;
+        public const ulong Nevada = 302240788186923019;
+        public const ulong NewMexico = 302240900615241729;
+        public const ulong Utah = 302240940418924544;
     }
 
     public static class SouthwestRoles
     {
-        public const ulong SWR = 0;
-        public const ulong CountryManager = 0;
-        public const ulong StateManager = 0;
-        public const ulong LargeAreaManager = 0;
-        public const ulong AreaManager = 0;
-        public const ulong Mentor = 0;
-        public const ulong Level6 = 0;
-        public const ulong Level5 = 0;
-        public const ulong Level4 = 0;
-        public const ulong Level3 = 0;
-        public const ulong Level2 = 0;
-        public const ulong Level1 = 0;
-        public const ulong Arizona = 0;
-        public const ulong California = 0;
-        public const ulong Colorado = 0;
-        public const ulong Hawaii = 0;
-        public const ulong Nevada = 0;
-        public const ulong NewMexico = 0;
-        public const ulong Utah = 0;
+        public const ulong SWR = 313652543529943040;
+        public const ulong CountryManager = 301115472953475072;
+        public const ulong StateManager = 301115500891734017;
+        public const ulong AreaManager = 301115538556321802;
+        public const ulong Mentor = 313653535327649792;
+        public const ulong Level6 = 301417011848937483;
+        public const ulong Level5 = 301417543317454849;
+        public const ulong Level4 = 301417571738189824;
+        public const ulong Level3 = 301417647591915531;
+        public const ulong Level2 = 301417611357454336;
+        public const ulong Level1 = 301417594458472449;
+        public const ulong Arizona = 313652270271168513;
+        public const ulong California = 313652320594296832;
+        public const ulong Colorado = 313652348662841345;
+        public const ulong Hawaii = 313652365440057344;
+        public const ulong Nevada = 313652381025959936;
+        public const ulong NewMexico = 313652403146850315;
+        public const ulong Utah = 313652425867132928;
     }
 }
