ca4d3b33d56690973da445b7ad88a2e0d8589709
Steven Hall <vaindil@gmail.com>
2017-05-15 08:35:28 -0400

    add NOR IDs

--- src/WazeBotDiscord/Regions/Northeast.cs
+++ src/WazeBotDiscord/Regions/Northeast.cs
@@ -2,32 +2,32 @@
 {
     public static class NortheastChannels
     {
-        public const ulong StateManagers = 0;
-        public const ulong LargeAreaManagers = 0;
-        public const ulong AreaManagers = 0;
-        public const ulong Delaware = 0;
-        public const ulong NewJersey = 0;
-        public const ulong NewYork = 0;
-        public const ulong Pennsylvania = 0;
+        public const ulong StateManagers = 313655484727033857;
+        public const ulong LargeAreaManagers = 313655443136446465;
+        public const ulong AreaManagers = 313655396235477003;
+        public const ulong Delaware = 303214247079575552;
+        public const ulong NewJersey = 302534681415516160;
+        public const ulong NewYork = 302534640244097024;
+        public const ulong Pennsylvania = 303214289718738944;
     }
 
     public static class NortheastRoles
     {
-        public const ulong NER = 0;
-        public const ulong CountryManager = 0;
-        public const ulong StateManager = 0;
-        public const ulong LargeAreaManager = 0;
-        public const ulong AreaManager = 0;
-        public const ulong Mentor = 0;
-        public const ulong Level6 = 0;
-        public const ulong Level5 = 0;
-        public const ulong Level4 = 0;
-        public const ulong Level3 = 0;
-        public const ulong Level2 = 0;
-        public const ulong Level1 = 0;
-        public const ulong Delaware = 0;
-        public const ulong NewJersey = 0;
-        public const ulong NewYork = 0;
-        public const ulong Pennsylvania = 0;
+        public const ulong NOR = 302529188521312257;
+        public const ulong CountryManager = 302528524802195457;
+        public const ulong StateManager = 302528666456555520;
+        public const ulong LargeAreaManager = 302528706042265602;
+        public const ulong AreaManager = 302528840901591041;
+        public const ulong Mentor = 302528989552181248;
+        public const ulong Level6 = 302529616260890627;
+        public const ulong Level5 = 302531740088664065;
+        public const ulong Level4 = 302531756932726784;
+        public const ulong Level3 = 302531769167511552;
+        public const ulong Level2 = 302531780567891968;
+        public const ulong Level1 = 302531790449410051;
+        public const ulong Delaware = 313654375010533376;
+        public const ulong NewJersey = 313654406841368577;
+        public const ulong NewYork = 313654448696328192;
+        public const ulong Pennsylvania = 313654465486127105;
     }
 }
