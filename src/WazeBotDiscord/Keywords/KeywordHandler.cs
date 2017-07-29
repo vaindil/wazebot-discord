1afc7bd1de86942c8837079857ae97ff736ce6ae
Steven Hall <vaindil@gmail.com>
2017-07-29 11:28:21 -0400

    list matched keywords in DM, remove mentions

--- src/WazeBotDiscord/Keywords/KeywordHandler.cs
+++ src/WazeBotDiscord/Keywords/KeywordHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Discord.WebSocket;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace WazeBotDiscord.Keywords
@@ -19,9 +20,32 @@ namespace WazeBotDiscord.Keywords
                     || !channel.Users.Any(u => u.Id == m.UserId))
                     continue;
 
+                var reply = new StringBuilder();
+                reply.Append(msg.Author.Username);
+                reply.Append(" mentioned ");
+                reply.Append(m.MatchedKeywords.Count);
+                reply.Append(" of your keywords in ");
+                reply.Append(msg.Channel.Name);
+                reply.Append(", ");
+                reply.Append(((SocketGuildChannel)msg.Channel).Guild.Name);
+                reply.Append(".\n\nMatched keyword");
+                if (m.MatchedKeywords.Count > 1)
+                    reply.Append("s");
+                reply.Append(":\n```\n");
+
+                foreach (var k in m.MatchedKeywords)
+                {
+                    reply.Append(k);
+                    reply.Append("\n");
+                }
+
+                reply.Append("```\n");
+                reply.Append("Message:\n```\n");
+                reply.Append(msg.Content);
+                reply.Append("\n```");
+
                 var dm = await client.GetUser(m.UserId).GetOrCreateDMChannelAsync();
-                await dm.SendMessageAsync($"{msg.Author.Mention} mentioned one or more of your keywords in " +
-                    $"{((SocketTextChannel)msg.Channel).Mention}.\n\n```{msg.Content}```");
+                await dm.SendMessageAsync(reply.ToString());
             }
         }
     }
