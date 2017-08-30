f744022b808e64f8e10350adb7f65e8f645ebe48
Steven Hall <vaindil@gmail.com>
2017-08-30 16:15:13 -0400

    MI fireside command

--- src/WazeBotDiscord/Modules/FiresideModule.cs
+++ src/WazeBotDiscord/Modules/FiresideModule.cs
@@ -1,4 +1,5 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 using Discord.WebSocket;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,31 +8,24 @@ namespace WazeBotDiscord.Modules
 {
     public class FiresideModule : ModuleBase
     {
-        [Command("fireside")]
-        public async Task ToggleFireside([Remainder]string unused = null)
+        [Command("mifireside")]
+        public async Task ToggleFireside(IUser user)
         {
-            var role = (SocketRole)Context.Guild.Roles.FirstOrDefault(r => r.Name == "Fireside");
-            if (role == null)
-            {
-                await ReplyAsync("This server doesn't have the Fireside role set up.");
-                return;
-            }
-
             var author = (SocketGuildUser)Context.Message.Author;
-            var msg = author.Username + ": Fireside role ";
 
-            if (author.Roles.Contains(role))
-            {
-                await author.RemoveRoleAsync(role);
-                msg += "removed.";
-            }
+            await Context.Message.DeleteAsync();
+
+            if (!author.Roles.Any(r => r.Id == 299566187791253504))
+                return;
+
+            if (author.Hierarchy < Context.Guild.GetRole(299566441751904258).Position)
+                return;
+
+            var channel = (SocketTextChannel)(await Context.Guild.GetChannelAsync(352541553543348224));
+            if (channel.GetPermissionOverwrite(user) != null)
+                await channel.RemovePermissionOverwriteAsync(user);
             else
-            {
-                await author.AddRoleAsync(role);
-                msg += "added.";
-            }
-
-            await ReplyAsync(msg);
+                await channel.AddPermissionOverwriteAsync(user, new OverwritePermissions(readMessages: PermValue.Allow));
         }
     }
 }
