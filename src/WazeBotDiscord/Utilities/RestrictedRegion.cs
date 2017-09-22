7c878133747781c5e280d381587d5d6997cd4b3b
Steven Hall <vaindil@gmail.com>
2017-09-21 21:58:12 -0400

    unrestrict MAR, #12

--- src/WazeBotDiscord/Utilities/RestrictedRegion.cs
+++ src/WazeBotDiscord/Utilities/RestrictedRegion.cs
@@ -4,9 +4,6 @@ namespace WazeBotDiscord.Utilities
 {
     public static class RestrictedRegion
     {
-        public static IReadOnlyList<ulong> Ids = new List<ulong>
-        {
-            299676784327393281  // MAR
-        };
+        public static IReadOnlyList<ulong> Ids = new List<ulong>();
     }
 }
